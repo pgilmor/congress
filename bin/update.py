2ff6e7d87631e87e09a32460bda2359c292f5272
Philip <pgilmor@gmail.com>
2018-12-26 11:25:41 -0600

    Update update.py

--- bin/update.py
+++ bin/update.py
@@ -2,7 +2,8 @@ import os
 import subprocess
 print os.environ['NAME']
 govinfoBashCommand = "./run govinfo --bulkdata=BILLSTATUS --congress=115 --debug"
+govinfo = govinfoBashCommand.split(" ")
 billsBashCommand = "./run bills --congress=115 --debug"
 votesBashCommand = "/run votes --congress=115 session=2018 --force --debug"
 
-subprocess.Popen("git init", shell=True)
+subprocess.call(govinfo)
