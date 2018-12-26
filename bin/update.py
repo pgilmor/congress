d1e53cbd2ce18953e851da74791d7889233482d5
Philip <pgilmor@gmail.com>
2018-12-26 12:29:44 -0600

    Update update.py

--- bin/update.py
+++ bin/update.py
@@ -1,9 +1,9 @@
 import os
 import subprocess
-import mysql.connector
+#import mysql.connector
 
 
-cnx = connection.MySQLConnection(user=os.environ['DBUSERNAME'], 
+#cnx = connection.MySQLConnection(user=os.environ['DBUSERNAME'], 
                                  password=os.environ['DBPASSWORD'],
                                  host=os.environ['DBHOST'],
                                  database=os.environ['DBNAME'])
@@ -17,8 +17,17 @@ bills = billsBashCommand.split(" ")
 votesBashCommand = "/run votes --congress=115 session=2018 --force --debug"
 votes = votesBashCommand.split(" ")
 
-# subprocess.call(govinfo)
-# subprocess.call(bills)
-# subprocess.call(votes)
+subprocess.call("git","init")
+subprocess.call("git","config", "user.name", os.environ['NAME'])
+subprocess.call("git","config", "user.email", os.environ['EMAIL'])
+subprocess.call("git","config", "github.user", os.environ['USERNAME'])
+subprocess.call("git","config", "github.token", os.environ['GITHUB'])
+subprocess.call("git","remote","add","https://github.com/pgilmor/congress.git")
+#subprocess.call(govinfo)
+subprocess.call(bills)
+#subprocess.call(votes)
+subprocess.call("git","add",".")
+subprocess.call("git","commit","-m","'data update'")
+subprocess.call("git","push","heroku","master")
 
-cnx.close()
+#cnx.close()
