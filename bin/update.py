fd768043578a48eeda672870341fa1ec76161b90
Philip <pgilmor@gmail.com>
2018-12-26 12:41:30 -0600

    Update update.py

--- bin/update.py
+++ bin/update.py
@@ -10,6 +10,13 @@ import subprocess
 
 
 print os.environ['NAME']
+name = os.environ['NAME']
+email = os.environ['EMAIL']
+githubUser = os.environ['USERNAME']
+token = os.environ['TOKEN']
+
+gitBashCommand = "git init && git config user.name {} && git config user.email {} && git config github.user {} && git config github.token {}".format(name, email, githubUser, token)
+print gitBashCommand
 govinfoBashCommand = "./run govinfo --bulkdata=BILLSTATUS --congress=115 --debug"
 govinfo = govinfoBashCommand.split(" ")
 billsBashCommand = "./run bills --congress=115 --debug"
@@ -17,17 +24,17 @@ bills = billsBashCommand.split(" ")
 votesBashCommand = "/run votes --congress=115 session=2018 --force --debug"
 votes = votesBashCommand.split(" ")
 
-subprocess.call("git","init")
-subprocess.call("git","config", "user.name", os.environ['NAME'])
-subprocess.call("git","config", "user.email", os.environ['EMAIL'])
-subprocess.call("git","config", "github.user", os.environ['USERNAME'])
-subprocess.call("git","config", "github.token", os.environ['GITHUB'])
-subprocess.call("git","remote","add","https://github.com/pgilmor/congress.git")
+#subprocess.call("git","init")
+#subprocess.call("git","config", "user.name", os.environ['NAME'])
+#subprocess.call("git","config", "user.email", os.environ['EMAIL'])
+#subprocess.call("git","config", "github.user", os.environ['USERNAME'])
+#subprocess.call("git","config", "github.token", os.environ['GITHUB'])
+#subprocess.call("git","remote","add","https://github.com/pgilmor/congress.git")
 #subprocess.call(govinfo)
-subprocess.call(bills)
+#subprocess.call(bills)
 #subprocess.call(votes)
-subprocess.call("git","add",".")
-subprocess.call("git","commit","-m","'data update'")
-subprocess.call("git","push","heroku","master")
+#subprocess.call("git","add",".")
+#subprocess.call("git","commit","-m","'data update'")
+#subprocess.call("git","push","heroku","master")
 
 #cnx.close()
