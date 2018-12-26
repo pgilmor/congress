740914e16c4318d1900cce55277e6c703b7c42ac
Philip <pgilmor@gmail.com>
2018-12-26 11:19:05 -0600

    Update update.py

--- bin/update.py
+++ bin/update.py
@@ -1,3 +1,8 @@
 import os
+import subprocess
 print os.environ['NAME']
+govinfoBashCommand = "./ run govinfo --bulkdata=BILLSTATUS --congress=115 --debug"
+billsBashCommand = "./run bills --congress=115 --debug"
+votesBashCommand = "/run votes --congress=115 session=2018 --force --debug"
 
+subprocess.run(govinfoBashCommand)
