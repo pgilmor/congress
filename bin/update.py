8a49e0d50a78823b82c294caee25bde34bfb4b2a
Philip <pgilmor@gmail.com>
2018-12-26 12:56:45 -0600

    Update update.py

--- bin/update.py
+++ bin/update.py
@@ -15,8 +15,10 @@ email = os.environ['EMAIL']
 githubUser = os.environ['USERNAME']
 token = os.environ['TOKEN']
 
-gitBashCommand = "git init && git config user.name {} && git config user.email {} && git config github.user {} && git config github.token {} && git remote add heroku https://github.com/pgilmor/congress.git".format(name, email, githubUser, token)
+gitBashCommand = "git init" 
+configBashCommand = "git config user.name {} && git config user.email {} && git config github.user {} && git config github.token {} && git remote add heroku https://github.com/pgilmor/congress.git".format(name, email, githubUser, token)
 git = gitBashCommand.split(" ")
+config = configBashCommand.split(" ")
 govinfoBashCommand = "./run govinfo --bulkdata=BILLSTATUS --congress=115 --debug"
 govinfo = govinfoBashCommand.split(" ")
 billsBashCommand = "./run bills --congress=115 --debug"
@@ -27,14 +29,14 @@ commitBashCommand ="git add . && git commit -m 'Update' && git push heroku maste
 commit = commitBashCommand.split(" ")
 
 subprocess.call(git)
-#subprocess.call("git","config", "user.name", os.environ['NAME'])
+subprocess.call(config)
 #subprocess.call("git","config", "user.email", os.environ['EMAIL'])
 #subprocess.call("git","config", "github.user", os.environ['USERNAME'])
 #subprocess.call("git","config", "github.token", os.environ['GITHUB'])
 #subprocess.call("git","remote","add","https://github.com/pgilmor/congress.git")
 #subprocess.call(govinfo)
-subprocess.call(bills)
-#subprocess.call(votes)
+#subprocess.call(bills)
+subprocess.call(votes)
 subprocess.call(commit)
 #subprocess.call("git","commit","-m","'data update'")
 #subprocess.call("git","push","heroku","master")
